8b81b4f48cc7a4c43f84aba8c7856f70ca81683f
vineeth <vineethkumar0005@gmail.com>
2024-04-13 18:47:31 +0530

    1.0 - Added Basic Models and Seeds
    '1' is a base version in which we tried to add the basic functionality. In version '2' we planned to add the advanced features like adding book requests and granting them by staff like that
    
    In this commit we added basic models and Seeds

--- models.py
+++ models.py
@@ -7,4 +7,37 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///"+os.path.join(basePath,'Libr
 db = SQLAlchemy(app)
 
 #write all the models here
+class BookLocation(db.Model):
+    rackID = db.Column(db.Integer,primary_key=True)
+    floorName = db.Column(db.String)
+    categoriesAvaliable = db.relationship('BookCategory',backref='locationDetails')
 
+class BookCategory(db.Model):
+    categoryID = db.Column(db.Integer,primary_key=True)
+    categoryName = db.Column(db.String)
+    rackID = db.Column(db.Integer,db.ForeignKey('book_location.rackID'),default=0)
+    booksPresent = db.relationship('Books',backref='categoryDetails')
+
+class Books(db.Model):
+    bookID = db.Column(db.Integer,primary_key=True,autoincrement=True)
+    bookName = db.Column(db.String)
+    bookCategory = db.Column(db.String,db.ForeignKey('book_category.categoryID'))
+    currentOwner = db.relationship('Users')
+
+class Roles(db.Model):
+    roleID = db.Column(db.Integer,primary_key=True)
+    roleName = db.Column(db.String,unique=True)
+    colleagues = db.relationship('Users',backref='roleDetails')
+
+class Users(db.Model):
+    userID = db.Column(db.Integer,primary_key=True,autoincrement=True)
+    userName = db.Column(db.String)
+    emailID = db.Column(db.String,unique=True)
+    password = db.Column(db.String)
+    role = db.Column(db.Integer,db.ForeignKey('roles.roleID'))
+    booksAssigned = db.Column(db.Integer,db.ForeignKey('books.bookID'))
+
+"""
+We need to create Three more Tables 'BookRequests','AssignedBooks','AvailableBooks'
+This is planned for next iteration
+"""
--- seed.py
+++ seed.py
@@ -18,6 +18,43 @@ def dbDrop():
 @app.cli.command('dbseed')
 def dbSeed():
     #write the seeding code here which loads the data to the data base tables
+    roles_date = [
+        {'roleId':1,'roleName':'Library Staff'},
+        {'roleId':2,'roleName':'Student'}
+    ]
+    book_location_data = [
+        {'rackID':0,'floorName':'ground Floor'},
+        {'rackID':1,'floorName':'ground Floor'},
+        {'rackID':2,'floorName':'First Floor'}
+    ]
+
+    book_category_data = [
+        {'categoryID':1,'categoryName':'Programming','rackID':1},
+        {'categoryID':2,'categoryName':'Self Help'},
+        {'categoryID':3,'categoryName':'Project Management','rackID':1}
+    ]
+
+    books_data = [
+        {'bookID':1,'bookName':'grooking Algorithms','bookCategory':1},
+        {'bookID':2,'bookName':'Let us C','bookCategory':1},
+        {'bookID':3,'bookName':'The magic of thinking big','bookCategory':2},
+        {'bookID':4,'bookName':'Atomic Habits','bookCategory':2},
+        {'bookID':5,'bookName':'GIT - The complete boot camp','bookCategory':3},
+        {'bookID':6,'bookName':'AGILE - Project Management','bookCategory':3}
+    ]
+
+    user_data = [
+        {'userID':1,'userName':'student1','emailID':'student1@clg.com','password':generate_password_hash('1234',method='pbkdf2:SHA256',salt_length=3),'role':2},
+        {'userID':2,'userName':'student2','emailID':'student2@clg.com','password':generate_password_hash('1234',method='pbkdf2:SHA256',salt_length=3),'role':2},
+        {'userID':3,'userName':'staff1','emailID':'staff1@clg.com','password':generate_password_hash('1234',method='pbkdf2:SHA256',salt_length=3),'role':1},
+        {'userID':4,'userName':'staff2','emailID':'staff2@clg.com','password':generate_password_hash('1234',method='pbkdf2:SHA256',salt_length=3),'role':1}
+    ]
+
+    db.session.bulk_insert_mappings(Roles,roles_date)
+    db.session.bulk_insert_mappings(BookLocation,book_location_data)
+    db.session.bulk_insert_mappings(BookCategory,book_category_data)
+    db.session.bulk_insert_mappings(Books,books_data)
+    db.session.bulk_insert_mappings(Users,user_data)
 
 
     db.session.commit()
